d8cad3e6140f0701600a37ef508c28af8678b79b
Leonardo <102773282+LeonardoJoseDaSilvaJunior@users.noreply.github.com>
2023-02-05 20:26:39 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,19 +5,10 @@ const botaoFecharModal = document.querySelector(".fechar-modal");
 const video = document.getElementById("video");
 const videoSrc = video.src;
 
-// ()=>{} == Criar função (de forma resumida)
-// function nomeFuncao(){} == criar função
-
-// toggle = se definido como false, o token será removido 
-// ex: const nome = "joao";
-// nome.classList.toggle("joao")
-// a joao já existe? sim, então o toggle irá o remover, 
-//caso não houvesse "joao", o toggle adicionaria; 
-
-
 function alternarModal(){
     modal.classList.toggle("aberto");
 }
+
 botaoTrailer.addEventListener("click", ()=>{
     alternarModal();
     modal.classList.add("aberto");
